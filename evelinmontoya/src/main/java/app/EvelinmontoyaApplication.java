b722d5e0be31eb3a92b6a12ae3711519d72e6866
evelinmh14 <evelin.montoya81@correo.tdea.edu.co>
2026-02-17 18:47:59 -0500

    commit de iniciacion

---
+++ evelinmontoya/src/main/java/app/EvelinmontoyaApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvelinmontoyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvelinmontoyaApplication.class, args);
+	}
+
+}
